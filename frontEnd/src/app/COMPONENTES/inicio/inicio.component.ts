05e314996fbe8fa32238d9713c566f972b6f60a7
Alland Naranjo <alland.naranjo@iteria.com.co>
2023-06-14 00:10:48 -0500

    Se cargan crud de usuarios

---
+++ frontEnd/src/app/COMPONENTES/inicio/inicio.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/SERVICE/usuario.service';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit{
+
+
+  constructor(private usuarioService: UsuarioService ){}
+
+  ngOnInit(): void {
+    this.listarUsuarios();  
+  }
+
+  listarUsuarios(){
+    this.usuarioService.getUsuarios().subscribe(
+       res=>{console.log(res);},
+       err => console.log(err)
+    );
+
+
+  }
+
+}
---
+++ frontEnd/src/app/SERVICE/usuario.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  url='http://localhost:8080/usuario';
+  constructor(private http: HttpClient) { }
+
+
+getUsuarios():Observable<any>
+{
+  return this.http.get(this.url);
+}
+
+getUnUsuario(id: String):Observable<any>
+{
+  return this.http.get(this.url+'/'+id);
+}
+
+saveUsuario(usuario : Usuario):Observable<any>{
+  return this.http.post(this.url , usuario);
+}
+
+editUsuario(id:string, usuario : Usuario ):Observable<any>
+{
+return this.http.put(this.url+'/'+id, usuario);
+}
+
+deleteUsuario(id:string):Observable<any>
+{
+  return this.http.delete(this.url+'/'+id);
+}
+
+
+}
+
+
+export interface Usuario{
+    id:string;
+    email:string;
+    estado:string;
+    nombre:string;
+    prioridad:string;
+
+}
